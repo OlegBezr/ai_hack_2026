f8d1d3501b596ccfc634c2356fe10f6118ad612d
Oleg Bezrukavnikov <obezrukavnikov@gmail.com>
2026-06-20 13:34:11 -0700

    sentry basic setup

--- dream_book/lib/main.dart
+++ dream_book/lib/main.dart
@@ -2,13 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'home_page.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Loads bundled dream_book/.env (MJ_ACCESS_TOKEN / MJ_REFRESH_TOKEN /
   // MJ_CLIENT_ID). isOptional => app still runs (interactive OAuth) if absent.
   await dotenv.load(fileName: '.env', isOptional: true);
-  runApp(const MainApp());
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://62962a3f26d98d6a44a02aac50bec1df@o4511599354118144.ingest.us.sentry.io/4511599650144256';
+      // Adds request headers and IP for users, for more info visit:
+      // https://docs.sentry.io/platforms/dart/guides/flutter/data-management/data-collected/
+      options.sendDefaultPii = true;
+      options.enableLogs = true;
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+      // Configure Session Replay
+      options.replay.sessionSampleRate = 0.1;
+      options.replay.onErrorSampleRate = 1.0;
+    },
+    appRunner: () => runApp(SentryWidget(child: const MainApp())),
+  );
+  // TODO: Remove this line after sending the first sample event to sentry.
+  await Sentry.captureException(StateError('This is a sample exception.'));
 }
 
 class MainApp extends StatelessWidget {
